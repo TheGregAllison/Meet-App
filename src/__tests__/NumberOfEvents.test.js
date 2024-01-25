b2ffbf53fbcfb48c5018d5fb96d249efa8f48dc2
TheGregAllison <146021687+TheGregAllison@users.noreply.github.com>
2024-01-25 16:53:47 -0600

    Code and tests for first 3 scenarios added
    Number of Events, EventList, Events, City Search, added, alongside additional code for App.js.

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents.js';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsComponent;
+  beforeEach(() => {
+    NumberOfEventsComponent = render(
+      <NumberOfEvents setNumberOfEvents={() => {}} />
+    );
+  });
+
+  test('contains textbox element', () => {
+    const numberTextBox = NumberOfEventsComponent.queryByRole('textbox');
+    expect(numberTextBox).toBeInTheDocument();
+  });
+
+  test('list contains 32 events by default', () => {
+    expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('32');
+  });
+
+  test('number of events updates when user types in textbox', async () => {
+    const numberOfEvents = NumberOfEventsComponent.queryByRole('textbox');
+    const user = userEvent.setup();
+    await user.type(numberOfEvents, '{backspace}{backspace}10');
+    expect(numberOfEvents).toHaveValue('10');
+  });
+});
